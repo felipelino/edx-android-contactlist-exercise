49f2e50fdc595c4e348ebc3ab0dbfec6ff7a620a
Felipe Lino <felipe.lino@b2wdigital.com>
2017-06-09 09:06:26 -0300

    Initial commit of exercise
    Done
    1) Download the base code from github repository
    2) Run the code on your computer
    3) Examine how parameters are sent from MainActivity to DetailActivity
    4) Send the rest of contact information to DetailActivity
    5) Load the parameters on DetailActivity on the OnCreate method
    
    Doing:
    6) Modify activity_detail.xml to show the rest of the contact information
    
    To do:
    7) Add support to your application for two different extra languages https://developer.android.com/guide/topics/resources/localization.html
    8) Apply styles, font colors, font sizes and any other style that might be consider necessary on activity_detail.xml. To get something similar to:

---
+++ app/src/main/java/com/beginner/micromaster/contactlist/DetailActivity.java
@@ -0,0 +1,27 @@
+package com.beginner.micromaster.contactlist;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+public class DetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+
+        TextView contactName = (TextView) findViewById(R.id.contactName);
+
+        Bundle b = new Bundle();
+        b = getIntent().getExtras();
+
+        //TODO: get the rest of contact parameters
+        String name = b.getString("name");
+        Contact contact = (Contact) b.get("contact");
+
+        //TODO: once you have all the values for a contact add them to the views
+        contactName.setText(name);
+
+    }
+}
